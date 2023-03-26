f2c4820d00c2319fe4b2f71e9c366738be8bc96c
Electro-Corp <chinmaytanmay@outlook.com>
2023-03-26 16:01:04 -0600

    add imgui maybe?

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-rm nexus
-g++ graphics/glad/src/glad.c graphics/gameObjects/gameObject.h graphics/shaders/shader.h exampleGame/main.cpp engine.h graphics/graphics.h graphics/textures/stb_image.h graphics/textures/texture.h graphics/gui/imgui.h physics/collider.h physics/collide_check.h libglfw3.a -fpermissive -lglfw -lGL -lGLU -lm -lX11 -lpthread -lXi -lXrandr -ldl -o nexus
+#rm nexus
+#g++ src/graphics/glad/src/glad.c src/graphics/gameObjects/gameObject.h src/graphics/shaders/shader.h exampleGame/main.cpp engine.h graphics/graphics.h graphics/textures/stb_image.h graphics/textures/texture.h graphics/gui/imgui.h physics/collider.h physics/collide_check.h libglfw3.a -fpermissive -lglfw -lGL -lGLU -lm -lX11 -lpthread -lXi -lXrandr -ldl -o nexus
+make
 ./nexus
